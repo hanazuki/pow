0ad5d6163a98a4116d62353b270ea96c23353463
Kasumi Hanazuki <kasumi@rollingapple.net>
2019-05-30 06:23:11 +0000

    --[no-]tls option instead of --scheme=

--- lib/pow/cli.rb
+++ lib/pow/cli.rb
@@ -8,14 +8,14 @@ module Pow
   class Cli < Thor
     map '-a' => :apply
 
-    desc 'apply ZONEFILE', 'apply zone changes'
+    desc 'apply [ZONEFILE]', 'Apply zone changes'
     method_option :dry_run, type: :boolean, default: false, desc: 'No actions. Just print simulation'
-    method_option :scheme, default: 'http', enum: %w[http https]
+    method_option :tls, type: :boolean, default: true, desc: 'Enable TLS'
     method_option :host, required: true
     method_option :port
-    def apply(zonefile)
+    def apply(zonefile = 'Zonefile')
       client = PDNS::Client.new(
-        scheme: options[:scheme],
+        scheme: options[:tls] ? 'https' : 'http',
         host: options[:host],
         port: options[:port],
         key: ENV.fetch('PDNS_API_KEY'),
